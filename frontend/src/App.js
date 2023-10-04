f48f61c4d8d43f585d268457446f1c39203539fc
Gremblom <cdflorezs@gmail.com>
2023-10-04 13:41:06 -0500

    arreglo de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 import BookCard from "./components/libros/Card";
@@ -16,6 +16,9 @@ function App() {
       <div className='main'>
         <div className='headerContainer'>
           <Header />
+        </div>
+        <div>
+          <Route exact path='/' component={BookCard} />
         </div>
         <div>
           <Route exact path='/libros' component={BookCard} />
